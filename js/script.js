f636c15fd13966d3dec17e6dae6592ede8f94dc1
Lev <yojirs.277@gmail.com>
2021-10-01 17:24:33 +0300

    add main files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+let inkr = '10',
+decr = '10';
+
+inkr++;
+decr--;
+
+console.log(inkr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const cheked = true,
+  closed = true;
+
+console.log(cheked && closed);
