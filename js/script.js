cd29403be77c7792bffcc67e92dc27b16b1a722f
Lev-Avhust <yojirs.277@gmail.com>
2021-10-03 16:06:09 +0300

    methods of strings and numbers

--- js/script.js
+++ js/script.js
@@ -68,3 +68,24 @@
 //     console.log ("1");
 //     return a + b;
 // };
+
+// const str = "test";
+// const arr = [1, 2, 3];
+
+console.log (str.length);
+console.log (str.toLocaleUpperCase());
+console.log (str.toLowerCase());
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("Some"));
+
+const logg = "Hello World!";
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 50.4;
+
+console.log(Math.round(num));
+console.log(parseInt(num));
+console.log(parseFloat(num));
