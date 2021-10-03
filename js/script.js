c34d359103adc752688b039631aa0aa26c725cf4
Lev-Avhust <yojirs.277@gmail.com>
2021-10-03 15:33:40 +0300

    function

--- js/script.js
+++ js/script.js
@@ -20,36 +20,51 @@
 // console.log(personalMovieDB);
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
         
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
 
-if (personalMovieDB.count <10) {
-    console.log ('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
-    console.log ('Неплохой результат');
-} else if (personalMovieDB.count >=30 && personalMovieDB.count <50) {
-    console.log ('Да ты киноман!');
-}
+// if (personalMovieDB.count <10) {
+//     console.log ('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
+//     console.log ('Неплохой результат');
+// } else if (personalMovieDB.count >=30 && personalMovieDB.count <50) {
+//     console.log ('Да ты киноман!');
+// }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
+// function showFirstMessage(text) {
+//     console.log(text);
+// }
+// showFirstMessage("Hello World!");
+
+// const logger = function() {
+//     console.log ("Hello!")
+// }
+
+// logger ();
+
+// const calc = (a, b) => {
+//     console.log ("1");
+//     return a + b;
+// };
