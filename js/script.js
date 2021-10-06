5a44a87b190703fd637268d0a967666ce5d9e433
Lev-Avhust <yojirs.277@gmail.com>
2021-10-07 01:04:00 +0300

    objects

--- js/script.js
+++ js/script.js
@@ -183,3 +183,49 @@
 // }
 
 // learnJS('JavaScript', done);
+
+// const options = {
+//     name: "test",
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: "black",
+//         bg: "red",
+//     }
+// };
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет знаечение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет знаечение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
+// const options = {
+//     name: "test",
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: "black",
+//         bg: "red",
+//     },
+//     makeTest: function() {
+//         console.log("Test");
+//     }
+// };
+
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length);
