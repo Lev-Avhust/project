138bd6b5273d875bbffa69b9170a1467ff3e04f9
Lev-Avhust <yojirs.277@gmail.com>
2021-10-07 16:04:08 +0300

    array

--- js/script.js
+++ js/script.js
@@ -229,3 +229,45 @@
 // console.log(border);
 
 // console.log(Object.keys(options).length);
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// arr[99] = 100;
+
+// console.log(arr.length);
+
+// console.log(arr);
+
+// arr.pop();
+// arr.push(10);
+// arr.unshift(0);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+
+// products.sort();
+// console.log(products.join("; "));
+
+// const arr = [1, 32, 31, 4, 25, 16];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum (a, b) {
+//     return a - b;
+// }
