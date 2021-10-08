420406f5e8a0912986e7cb15c626cf438b9272e6
Lev-Avhust <yojirs.277@gmail.com>
2021-10-09 02:01:51 +0300

    transfer of data types

--- js/script.js
+++ js/script.js
@@ -487,3 +487,50 @@
 
 
 
+// to String 
+
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+// console.log(typeof(5 + ""));
+
+// const num = 5;
+
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + 'px';
+
+// console.log(fontSize);
+
+// // to Number
+
+// console.log(typeof(Number('4')));
+
+// console.log(typeof(+'5'));
+
+// console.log(typeof(parseInt('4px', 10)));
+
+// let answ = +prompt("Hello!", "");
+
+// to Boolean
+
+// 0, '', null, undefined, NaN - false
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// switcher = 1;
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// console.log(typeof(Boolean('4')));
+
+
+
+
+
+
