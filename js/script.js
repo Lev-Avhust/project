973eca68e737fb2e068cf1b1cd5d9cbf87719a73
Lev-Avhust <yojirs.277@gmail.com>
2021-10-10 15:31:05 +0300

    get elements

--- js/script.js
+++ js/script.js
@@ -530,7 +530,32 @@
 // console.log(typeof(Boolean('4')));
 
 
+// alert(typeof("1"[0]));
 
+// const box = document.getElementById("box");
 
+// console.log(box);
+
+// const btns = document.getElementsByTagName("button")[1];
+
+// console.log(btns);
+
+// const circles = document.getElementsByClassName("circle");
+
+// console.log(circles[1]);
+
+// const hearts = document.querySelectorAll(".heart");
+
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+
+// const oneHeart = document.querySelector(".heart");
+
+// console.log(oneHeart);
+
+// const div = document.querySelector("div");
+
+// console.log(div);
 
 
