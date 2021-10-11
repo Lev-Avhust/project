c1a495e4b6132eb0ca7a213aa14fab8d9150736c
Lev-Avhust <yojirs.277@gmail.com>
2021-10-12 01:07:27 +0300

    DOM navigations data-atributes

--- js/script.js
+++ js/script.js
@@ -688,6 +688,30 @@
 //     console.log(e.target);
 // });
 
+// console.log(document.body);
+
+// console.log(document.documentElement);
+
+// console.log(document.body.childNodes);
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector("[data-current='3']").parentElement.parentElement);
+
+// console.log(document.querySelector("[data-current='3']").nextElementSibling);
+
+// console.log(document.querySelector("[data-current='3']").previousElementSibling);
+
+// for (const node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     }
+//     console.log(node);
+// }
+
 
 
 
