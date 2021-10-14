f76e61a4753475361904d163f92fdea08c11a74d
Lev-Avhust <yojirs.277@gmail.com>
2021-10-14 12:16:50 +0300

    classList

--- js/script.js
+++ js/script.js
@@ -828,14 +828,14 @@
 //         });
 
         
-//         document.querySelectorAll('.delete').forEach((btn, i) => {
-//             btn.addEventListener('click', () => {
-//                 btn.parentElement.remove();
-//                 movieDB.movies.splice(i, 1);
+        // document.querySelectorAll('.delete').forEach((btn, i) => {
+        //     btn.addEventListener('click', () => {
+        //         btn.parentElement.remove();
+        //         movieDB.movies.splice(i, 1);
 
-//                 createMovieList(films, parent);
-//             });
-//         });
+        //         createMovieList(films, parent);
+        //     });
+        // });
 //     }  
     
 //     createMovieList(movieDB.movies, movieList);
@@ -843,4 +843,39 @@
 // });
 
 
+// const btns = document.querySelectorAll("button"),
+//       wrapper = document.querySelector('.btn-block');  
+// console.log(btns[0].classList.length); 
+// console.log(btns[0].classList.item(0)); 
+// console.log(btns[1].classList.add("red")); 
+// console.log(btns[0].classList.remove("blue")); 
+// console.log(btns[0].classList.toggle("blue")); 
 
+// if (btns[1].classList.contains("red")) {
+//     console.log('red');
+// }
+
+// btns[0].addEventListener('click', () => {
+    // if(!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+//     btns[1].classList.toggle('red');
+// });
+
+// wrapper.addEventListener('click', (e) => {
+//     if (e.target && e.target.matches('button.red')) {
+//         console.log('hello!');
+//     }
+// });
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('hello!');
+//     }); 
+// });
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
