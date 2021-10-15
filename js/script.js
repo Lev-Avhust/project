021aabad0b1a23dbfe7a3ad4243144d65f3dd0e4
Lev-Avhust <yojirs.277@gmail.com>
2021-10-16 00:07:41 +0300

    Date

--- js/script.js
+++ js/script.js
@@ -973,3 +973,32 @@
 //     id = setTimeout(log, 500);
     
 // }, 500);
+
+// const now = new Date(1634324313468);
+// new Date.parse('1634324313468')
+
+// console.log(Data);
+
+// console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+
+// console.log(now.getTime());
+
+// let start = new Date();
+
+// for (let i = 0; i < 100000; i++) {
+//     let some = i ** 3;
+// } 
+
+// let end = new Date();
+
+
+// alert(`Цикл отработал за ${start - end} секунд`);
+
