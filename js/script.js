0581830a2041f5e664d7050101c384d4a8ea1949
Lev-Avhust <yojirs.277@gmail.com>
2021-11-01 16:37:18 +0200

    modulations

--- js/script.js
+++ js/script.js
@@ -1472,6 +1472,27 @@
 
 // ivan.say();
 
+const number = 1;
+
+(function(){
+  let number = 2;
+  console.log(number);
+}());
+
+console.log(number);
+
+const user = (function(){
+  const privat = function() {
+    console.log('I am private');
+  };
+
+  return {
+    sayHello: privat
+  };
+}());
+
+user.sayHello();
+
 
 window.addEventListener('DOMContentLoaded', () => {
 
